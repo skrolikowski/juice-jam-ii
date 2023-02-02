61bc4524478ab3d537bde49e22f31b64db6c8847
Shane Krolikowski <shane@houseofhow.com>
2023-02-01 18:20:06 -0800

    Adding juice

--- src/toolbox/graphics/init.lua
+++ src/toolbox/graphics/init.lua
@@ -2,6 +2,17 @@ Spritesheet = require "src.toolbox.graphics.spritesheet"
 Animation   = require "src.toolbox.graphics.animation"
 --
 Sheet       = {
-    Symbol = Spritesheet("res/image/sheet/symbol.json"),
-    Apple  = Spritesheet("res/image/sheet/Apple.json"),
+    Symbol     = Spritesheet("res/image/sheet/symbol.json"),
+    Apple      = Spritesheet("res/image/sheet/Apple.json"),
+    Coin       = Spritesheet("res/image/sheet/Coin.json"),
+    Chest      = Spritesheet("res/image/sheet/Chest.json"),
+    Coin_End   = Spritesheet("res/image/sheet/Coin_End.json"),
+    Diamond    = Spritesheet("res/image/sheet/Diamond.json"),
+    Heart_Heal = Spritesheet("res/image/sheet/Heart_Heal.json"),
+    Heart_Hurt = Spritesheet("res/image/sheet/Heart_Hurt.json"),
+    Ingot      = Spritesheet("res/image/sheet/Ingot.json"),
+    Potion     = Spritesheet("res/image/sheet/Potion.json"),
+    Shield     = Spritesheet("res/image/sheet/Shield.json"),
+    Skull      = Spritesheet("res/image/sheet/Skull.json"),
+    Skull_End  = Spritesheet("res/image/sheet/Skull_End.json"),
 }
